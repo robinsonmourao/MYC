59fd79d476fbda82ac0f4961adffcf7e0760b750
Robinson Mourão <bob.info.guaratiba@gmail.com>
2024-02-14 09:19:23 -0300

    Initial commit

---
+++ src/main/java/com/example/MYC/MycApplication.java
@@ -0,0 +1,13 @@
+package com.example.MYC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MycApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MycApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/MYC/MycApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MYC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MycApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
