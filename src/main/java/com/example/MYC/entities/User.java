e668b97e43b127f67509b721beba1f50dff3fc87
Robinson Mourão <bob.info.guaratiba@gmail.com>
2024-02-16 10:30:04 -0300

    fixing connection to PostgreSQL

--- src/main/java/com/example/MYC/entities/User.java
+++ src/main/java/com/example/MYC/entities/User.java
@@ -1,14 +1,12 @@
 package com.example.MYC.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 import java.io.Serializable;
 import java.util.Objects;
 
 @Entity
+@Table(name="myc_user")
 public class User implements Serializable {
 
     @Id
